c61a28eb6be729065cc6d3574beca8a9a1b8e097
atlasfox007 <alexandro.tobing@gmail.com>
2021-04-05 18:52:00 +0700

    marker show when clicked

--- maps/index.js
+++ maps/index.js
@@ -3,7 +3,7 @@ function initMap() {
     const monas = { lat: -6.175252901681506, lng: 106.82713921415032 };
     // The map, centered at monas
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 17,
+      zoom: 10,
       center: monas,
     });
     // The marker, positioned at monas
@@ -11,4 +11,33 @@ function initMap() {
       position: monas,
       map: map,
     });
+
+    // METHOD
+
+    // ZOOM KETIKA DI CLICK
+    map.addListener("center_changed", () => {
+      // 3 seconds after the center of the map has changed, pan back to the
+      // marker.
+      window.setTimeout(() => {
+        map.panTo(marker.getPosition());
+      }, 3000);
+    });
+
+    marker.addListener("click", () => {
+      map.setZoom(16);
+      map.setCenter(marker.getPosition());
+    });
+
+
+    map.addListener("click", (e) => {
+      placeMarkerAndPanTo(e.latLng, map);
+    });
+  }
+
+  function placeMarkerAndPanTo(latLng, map) {
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+    map.panTo(latLng);
   }
