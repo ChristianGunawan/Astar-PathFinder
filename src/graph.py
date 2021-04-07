e009d35202b3ab2a7ad2dd8c24d7850766f2a3e1
Christian Gunawan <57822261+ChristianGunawan@users.noreply.github.com>
2021-04-07 11:03:53 +0700

    Add files via upload

---
+++ src/graph.py
@@ -0,0 +1,47 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from read_input import coordinateDict
+from PathFinder import Astar, treeDict
+
+
+def graphMaker(start, goal):
+    # Inisialisasi Graph
+    G = nx.Graph()
+    # Initialize variables from PathFinder
+    visited_graph, ordered_sequence, cost_function = Astar(start, goal)
+    # Jarak
+    distance_sum = 0
+    for i in range(len(ordered_sequence) - 1):
+        distance_sum += treeDict[ordered_sequence[i]][ordered_sequence[i + 1]]
+    # Bentuk semua nodes
+    for nodes in treeDict:
+        G.add_node(nodes, pos = (coordinateDict[nodes]['lat'], coordinateDict[nodes]['lng']))
+    # Bentuk semua edges
+    for nodes in treeDict:
+        for children in treeDict[nodes]:
+            G.add_edge(nodes, children, weight = treeDict[nodes][children])
+    
+    # Posisi
+    pos = nx.get_node_attributes(G, 'pos')
+    # Bobot
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G,pos, edge_labels= labels)
+    # Mewarnai node yang dikunjungi
+    node_color = []
+    for node in G.nodes:
+        if node in ordered_sequence:
+            node_color.append("red")
+        else :
+            node_color.append("blue")
+        
+    nx.draw(G, pos , with_labels = True, node_size = 1200,  node_color = node_color)
+    for i in range(len(ordered_sequence)):
+        if(i != len(ordered_sequence) - 1):
+            print(f"{ordered_sequence[i]} =>", end = " ")
+        else:
+            print(ordered_sequence[i])
+    if distance_sum == 0 :
+        print("Jalur tidak ditemukan atau menunjuk ke diri sendiri")
+    else :
+        print(f"Panjang lintasan adalah {distance_sum}")
+    plt.show()
---
+++ src/read_input.py
@@ -0,0 +1,87 @@
+import os
+from collections import defaultdict
+from os.path import dirname, abspath
+    
+# def read_input():
+#     directory = dirname(abspath(__file__))
+#     namafile = str(input("Masukkan nama file : "))
+#     pathfile = os.path.join(directory, 'test\\' + namafile + '.txt')
+#     f = open(pathfile,"r")
+
+#     # Parsing The input
+#     """
+#     Input : Connection between places with distance, and heuristic values
+#     Output : adjacency matrix and dictionary of heuristic values per places
+#     Guide : Index 0 for pathDictionary index 1 for heuristic Dictionary
+#     """
+#     pathDict = defaultdict(dict)
+#     heuristicDict = dict()
+#     oneWayPathDict = defaultdict(dict)
+#     toParse = f.readlines()
+
+#     #Parsing nilai heurisitik
+#     toParseHeuristik = toParse[toParse.index("HEURISTIK\n") + 1:]
+#     for word in toParseHeuristik:
+#         heuristicParse = word.replace("\n", "").split(" ")
+#         heuristicDict[heuristicParse[0]] = int(heuristicParse[1])
+#     #Parsing nilai jalur 
+#     toParseJalur = toParse[:toParse.index("HEURISTIK\n")]
+#     for word in toParseJalur:
+#         wordParse = word.replace("\n", "").split(" ")
+#         # Jalur dianggap bisa 2 arah
+#         pathDict[wordParse[0]][wordParse[1]] = int(wordParse[2])
+#         pathDict[wordParse[1]][wordParse[0]] = int(wordParse[2])
+#         oneWayPathDict[wordParse[0]][wordParse[1]] = int(wordParse[2])
+    
+#     return pathDict, heuristicDict, oneWayPathDict
+
+coordinateDict = defaultdict(dict)
+def read_input2():
+    directory = dirname(dirname(abspath(__file__)))
+    namafile = str(input("Masukkan nama file : "))
+    pathfile = os.path.join(directory, 'test\\' + namafile + '.txt')
+    f = open(pathfile,"r")
+
+    # Parsing The input
+    """
+    Input : Connection between places with distance, and heuristic values
+    Output : adjacency matrix and dictionary of heuristic values per places
+    Guide : Index 0 for pathDictionary index 1 for heuristic Dictionary
+    """
+    weightDict = defaultdict(dict)
+    
+    # Mendapatkan jumlah nodes
+    toParse = f.readline()
+    jumlahNodes = int(toParse)
+
+    # Mendapatkan list dari seluruh tempat, PENTING !
+    parsedNodes = f.readline().replace("\n", "").split(" ")
+
+    # Parsing koordinat
+    toParse = f.readlines()
+    koordinatParse = toParse[:toParse.index("MATRIKS\n")]
+    for placeCoordinate in koordinatParse:
+        parsed = placeCoordinate.replace("\n","").split(" ")
+        coordinateDict[parsed[0]]["lat"] = float(parsed[1])
+        coordinateDict[parsed[0]]["lng"] = float(parsed[2])
+
+    # Parsing matriks
+    MATRIKS = []
+    matrixParse = toParse[toParse.index("MATRIKS\n") + 1:]
+    for elem in matrixParse:
+        elemParse = elem.replace("\n", "").split(" ")
+        elemParse = list(map(int, elemParse))
+        MATRIKS.append(elemParse)
+
+    # Parsing bobot
+    for parent in range(jumlahNodes):
+        for children in range(jumlahNodes):
+            if (MATRIKS[parent][children] == 1):
+                weightDict[parsedNodes[parent]][parsedNodes[children]] = euclideanDistance(parsedNodes[parent], parsedNodes[children])
+    return weightDict, coordinateDict
+
+def euclideanDistance(start, end):
+    powerDifLat = (coordinateDict[start]["lat"] - coordinateDict[end]["lat"])**2
+    powerDifLng = (coordinateDict[start]["lng"] - coordinateDict[end]["lng"])**2
+    return round((powerDifLat + powerDifLng) ** (1/2), 3)
+
